95f66e8b799ca7bfaa3acb706c96abd44c777554
Katia <katerina.bogrom@gmail.com>
2021-08-24 09:48:06 +0200

    T26 Almacenes con H2

---
+++ src/main/java/mainApp/T26AlmacenesConH2Application.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26AlmacenesConH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26AlmacenesConH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/mainApp/T26AlmacenesConH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T26AlmacenesConH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
